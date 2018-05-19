890d671c8083fb600628db444595235d1f386537
janella <janella.espinas@gmail.com>
2018-05-19 20:25:10 +1200

    Base solution commit

---
+++ src/SolveTheBreach/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open SolveTheBreach.Libs
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Say.hello "Janella"
+    0 // return an integer exit code
